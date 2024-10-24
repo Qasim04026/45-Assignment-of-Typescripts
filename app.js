367938519f7671bee13a1347e0881c164b2d57b4
Qasim04026 <161979120+Qasim04026@users.noreply.github.com>
2024-10-25 01:36:18 +0500

    Assignment 3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Question # 2
+// Question # 3
 //Name Cases: Store a person's name in a variable and than print that person's name in lowercase,uppercase and titlecase.
 var personName = "Qasim Raza";
 // in lowercase
